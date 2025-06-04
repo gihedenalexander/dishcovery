aca658d33d7672325e48a7c40f04eaa06c4dac37
Nicklas02 <Nicklasc.ronaldo@gmail.com>
2025-06-04 11:44:45 +0200

    Implementerade funktionalitet för att spara undan favoritmaträtter och deras fakta till en ny sida samt till LocalStorage
    Co-Authored-By: Mattias Malm <128680479+malmmattias@users.noreply.github.com>

--- dishcovery/src/FoodItem.jsx
+++ dishcovery/src/FoodItem.jsx
@@ -4,7 +4,6 @@ function FoodItem({ name }) {
   const [fact, setFact] = useState(null);
   const [loading, setLoading] = useState(false);
 
-
   const fetchFunFact = async () => {
     setLoading(true);
     setFact(null);
@@ -17,7 +16,6 @@ function FoodItem({ name }) {
         },
         body: JSON.stringify({ name }),
       });
-
       const data = await response.json();
       setFact(data.fact || "No fact was found!");
     } catch (error) {
@@ -27,16 +25,36 @@ function FoodItem({ name }) {
     }
   };
 
+  const addToFavorites = () => {
+    const favoriteFood = JSON.parse(localStorage.getItem("favorites")) || [];
+    const newFavorite = { name, fact };
+
+      favoriteFood.push(newFavorite);
+      localStorage.setItem("favorites", JSON.stringify(favoriteFood));
+      console.log(`Added to favorites: ${name}`);
+      alert(`"${name}" fact has been added to your favorites`);
+  };
+
   return (
     <li className="list-group-item d-flex justify-content-between align-items-center">
-        <strong>{name}</strong>  {fact && <p>{fact}</p>}
-      <button
-        className="btn btn-primary"
-        onClick={fetchFunFact}
-        disabled={loading}
-      >
-            {loading ? "Loading..." : "Show intriguing fact"} {" "}
-      </button>
+      <div>
+        <strong>{name}</strong>
+        {fact && <p className="mt-2 mb-0">{fact}</p>}
+      </div>
+      <div className="d-flex gap-2">
+        <button
+          className="btn btn-primary"
+          onClick={fetchFunFact}
+          disabled={loading}
+        >
+          {loading ? "Loading..." : "Show intriguing fact"}
+        </button>
+        {fact && (
+          <button className="btn btn-favorite btn-success" onClick={addToFavorites}>
+            Add to favorites
+          </button>
+        )}
+      </div>
     </li>
   );
 }
